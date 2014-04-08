409230136dd79e9ae86f3634d3a42412f36e6fb4
Sappha O'Meara <sappha.omeara@gmail.com>
2014-04-08 18:54:07 -0400

    Finish working code

--- 12-Stack/MyStack.java
+++ 12-Stack/MyStack.java
@@ -1,4 +1,48 @@
-public class MyStack{
+import java.util.*;
+import java.io.*;
 
+public class MyStack{
+    private int[] list;
+    private int top;
+
+    public MyStack(){
+	top = 0;
+	list = new int[10];
+    }
+
+    public void push(int s){
+	if(top == list.length){
+	    int[] tmp = new int[list.length * 2];
+	    for(int i = 0; i < list.length; i++){
+		tmp[i] = list[i];
+	    }
+	    list = tmp;
+	}
+	list[top] = s;
+	top++;
+    }
+
+    public int pop(){
+	int s = list[top - 1];
+	list[top - 1] = 0;
+	top--; 
+	return s;
+    }
+
+    public int peek(){
+	return list[top - 1];
+    }
+
+    public boolean isEmpty(){
+	return list[0] == 0;
+    }
+
+    public String toString(){
+	String s = "";
+	for(int i = 0; i < top; i++){
+	    s += list[i] + ", ";
+	}
+	return s;
+    }
 
 }
